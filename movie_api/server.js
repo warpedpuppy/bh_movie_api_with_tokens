e460718f2401c691746af05a6bb9042c1de1a1b7
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2021-07-20 07:48:32 -0400

    line 8 changed file path name

--- movie_api/server.js
+++ movie_api/server.js
@@ -5,7 +5,7 @@ const http = require('http'),
     http.createServer((request, response) => {
         let addr = request.url,
         q = url.parse(addr, true),
-        filePath = '';
+        filePath = 'movie_api';
 
         fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
             if(err) {
