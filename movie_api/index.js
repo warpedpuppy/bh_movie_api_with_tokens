812a2b31e4eeab8391b9d7cca6ab230b148dc9d8
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2021-07-21 19:07:21 -0400

    created topmovies array and imported express

---
+++ movie_api/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+
+let topmovies = [
+   { title: 'Batman',
+    genre: Action
+},
+    {
+      title:'Indiana Jones and the Last Crusade',
+      genre:'Lethal Weapon 2'  
+    },
+    {
+        title:'Honey, I Shrunk the Kids',
+        genre:'Adventure'
+    },
+    {
+        title:'Rain Man',
+        genre:'Comedy'
+    },
+    {
+        title:'Look Who"'"s Talking',
+        genre: 'Comedy'
+    },
+    {
+        title: 'Ghostbusters II',
+        genre: 'Comedy'
+    }
+    {
+        title: 'Back to the Future Part II',
+        genre: 'Adventure'
+    },
+    {
+        title: 'Parenthood',
+        genre: 'Comedy'
+    },
+    {
+        title: 'Dead Poets Society',
+        genre: 'Drama'
+    }
+];
