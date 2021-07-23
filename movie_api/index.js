1ff51b674277d0eebd4cf825863943c724552c8c
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2021-07-23 12:10:12 -0400

    added more details to response body

--- movie_api/index.js
+++ movie_api/index.js
@@ -47,9 +47,70 @@ let topmovies = [
         res.json(topmovies);
     });
 
+    app.get('/movies[directors]', (req, res) => {
+        res.json('/movies/:directors', (req, res) => { return movies.directors === req.params.directors});
+    });
+
+    app.get(/user, (req, res) => {
+        res.send('Successful get request returning data on users')
+    });
+
     app.get('/', (req, res) => {
         res.send('This is a test!');
     });
+
+    //add movies//
+    app.post('/movies', (req, res) => {
+        let newMovies = req.body;
+
+        if (!newMovies.name) {
+            const message = 'Missing name in request body';
+            res.status(400).send(message);
+        } else {
+            newMovies.id = uuid.v4();
+            movies.push(newMovies);
+            res.status(201).send(newMovies);
+        }
+    });
+
+    //delete movies//
+    app.delete('/movies/:id', (req, res) => {
+        let movies = movies.find((movies) => {
+            return movies.id === req.params.id }); 
+    
+        if (student) {
+            students = students.filter((obj) => {
+                return obj.id !== req.params.id });
+            res.status(201).send('movies ' + req.params.id + 'was deleted.');
+            }
+    });
+
+
+    //add users//
+    app.post('/movies/:user', (req, res) => {
+        let newUser = req.body;
+
+        if (!newUser) {
+            const message = 'Missin name in request body';
+            res.status(400).send(message);
+        } else {
+            newUser.id = uuid.v4();
+            res.status(201).send(newStudent);
+        }
+    });
+
+
+    //delete users//
+    app.delete('/movies/:user', (req, res) => {
+        let user = user.find((obj) => { return obj.id !== req.params.id});
+        res.status(201).send('User ' + req.params.id + 'was deleted. ');
+        }
+    });
+
+    //update movies//
+    app.put('/movies/:name/:genre/:description/:image', (req, res) => {
+        let student = students.find((student) => { return movies})
+    })
 
     app.use('/documentation.html', express.static('public'));
 
