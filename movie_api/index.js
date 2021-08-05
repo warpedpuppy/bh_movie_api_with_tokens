3d96826a06d6c80c147c71cb677095f786d4d474
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2021-08-05 07:36:21 -0400

    deleted extra movie list

--- movie_api/index.js
+++ movie_api/index.js
@@ -5,44 +5,7 @@ const express = require('express'),
         uuid = require('uuid');
 
 
-        let topMovies = [
-             { 
-                title: 'Batman',
-                genre: 'Action'
-             },
-             {
-               title:'Indiana Jones and the Last Crusade',
-               genre:'Lethal Weapon 2'  
-             },
-             {
-                 title:'Honey, I Shrunk the Kids',
-                 genre:'Adventure'
-             },
-             {
-                 title:'Rain Man',
-                 genre:'Comedy'
-             },
-             {
-                 title:'Look Whos Talking',
-                 genre: 'Comedy'
-             },
-             {
-                 title: 'Ghostbusters II',
-                 genre: 'Comedy'
-             },
-             {
-                 title: 'Back to the Future Part II',
-                 genre: 'Adventure'
-             },
-             {
-                 title: 'Parenthood',
-                 genre: 'Comedy'
-             },
-             {
-                 title: 'Dead Poets Society',
-                 genre: 'Drama'
-             }
-        ];
+    
 
         let movies = [
             {
