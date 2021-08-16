13b1fabc5d5a7d9afa19ab4ad89f19f9a7c85816
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2021-08-16 07:31:04 -0400

    added CORS

--- index.js
+++ index.js
@@ -28,6 +28,19 @@ const { Movie } = require('./models.js');
 
     app.use(bodyParser.urlencoded({ extended: true }));
 
+    //------Added Cross Origin Resource Sharing-----
+    const cors = require('cors');
+    app.use(cors({
+        origin: (origin, callback) => {
+            if(!origin) return callback(null, true);
+            if(allowedOrigins.indexOf(origin) === -1){
+                let message = 'The CORS policy for this application does not allow access from orgin ' + origin;
+                return callback(new Error(message ), false);
+            }
+            return callback(null, true);
+        }
+    }));
+
     let auth = require('./auth')(app);
     const passport = require('passport');
     require('./passport');
